ccfdd1d787a6521410518ce70a9f08eff7c8205f
GiuseppeM <peppemartinelli66@gmail.com>
2025-05-13 12:57:49 +0200

    Added custom errors in errors.py and most of the unit tests for blockchain.py

---
+++ src/blockchain/errors.py
@@ -0,0 +1,21 @@
+"""Custom errors for the blockchain.py module"""
+
+
+class InvalidChain(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class NoTransactionsFound(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class InvalidTransactions(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class ContractNotFound(Exception):
+    def __init__(self, message):
+        super().__init__(message)
--- src/blockchain/test/test_blockchain.py
+++ src/blockchain/test/test_blockchain.py
@@ -1,10 +1,15 @@
 import datetime
 
+import pytest
+
 from ..blockchain import BlockChain, Block
 from ..smart_contract import SmartContract
+from ..errors import ContractNotFound, NoTransactionsFound
 from datetime import datetime
+from copy import deepcopy
 
 blockchain = BlockChain(difficulty=3)
+tr = [{"data": [], "is_contract": False, "contract_address": ""}]
 
 
 def test_genesis_block_creation():
@@ -19,7 +24,34 @@ def test_create_passed_genesis_block():
         proof=100,
         transactions=[],
     )
-    local_chain = BlockChain(difficulty=3)
+    local_chain = BlockChain(difficulty=3, genesis_block=genesis)
+    assert len(local_chain.chain) == 1
+    assert local_chain.chain[-1] == genesis
+
+
+def test_mine_no_committed_transactions():
+    global blockchain
+    with pytest.raises(NoTransactionsFound) as error:
+        blockchain.mine()
+
+
+def test_mine_check_contract_address_creation():
+    global blockchain, tr
+
+    def test_function(*args):
+        pass
+
+    test_tr = deepcopy(tr)
+    test_tr[0]["data"] = SmartContract.encode(test_function)
+    blockchain.add_new_transaction(
+        [{"data": test_tr, "is_contract": True, "contract_address": ""}]
+    )
+    assert blockchain.mine()
+    assert len(blockchain.chain) > 1
+    assert blockchain.chain[-1].transactions[0]["contract_address"]
+
+    # Cleanup
+    del blockchain.chain[1:]
 
 
 def test_smart_contract_exec():
@@ -39,3 +71,63 @@ def test_smart_contract_exec():
     )
     assert blockchain.mine()
     assert len(blockchain.chain) > 2
+    # Cleanup
+    del blockchain.chain[1:]
+
+
+def test_smart_contract_exec_data_modified():
+    global blockchain
+
+    def function_that_modifies_transaction_data(data: list):
+        for i in range(len(data)):
+            data[i] += 2
+
+    encoded: str = SmartContract.encode(function_that_modifies_transaction_data)
+    blockchain.add_new_transaction(
+        [{"data": [encoded], "is_contract": True, "contract_address": ""}]
+    )
+    assert blockchain.mine()
+    assert len(blockchain.chain) > 1
+    address = blockchain.chain[-1].transactions[0]["contract_address"]
+    blockchain.add_new_transaction(
+        [{"data": [0] * 5, "is_contract": False, "contract_address": address}]
+    )
+    assert blockchain.mine()
+    assert blockchain.chain[-1].transactions[0]["data"] == [2] * 5
+    assert len(blockchain.chain) > 1
+
+    # Cleanup
+    del blockchain.chain[1:]
+
+
+def test_mine_contract_not_found():
+    global blockchain
+    blockchain.add_new_transaction(
+        [
+            {
+                "data": [0] * 5,
+                "is_contract": False,
+                "contract_address": "not a valid address",
+            }
+        ]
+    )
+    with pytest.raises(ContractNotFound) as error:
+        blockchain.mine()
+    assert len(blockchain.chain) == 1
+
+
+def test_check_is_chain_valid_good_chain():
+    global blockchain
+    blockchain.unconfirmed_transactions = []
+    for i in range(5):
+        blockchain.add_new_transaction(
+            [
+                {
+                    "data": f"test{i}",
+                    "is_contract": False,
+                    "contract_address": "",
+                }
+            ]
+        )
+        blockchain.mine()
+    assert blockchain.is_chain_valid()
